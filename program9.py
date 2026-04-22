09bb6affe7a30de0c50bf2b013a790401afc1a25
Ajnish <ajnishroy86@gmail.com>
2026-04-23 00:15:40 +0530

    Update program9.py

--- program9.py
+++ program9.py
@@ -1,3 +1,15 @@
+# Module: shapes.py
+import math
+
+def area_circle(radius):
+    return math.pi * radius * radius
+
+def area_rectangle(length, width):
+    return length * width
+
+def area_triangle(length, width):
+    return 0.5 * length * width
+
 # Main Program: main.py
 import shapes
 
